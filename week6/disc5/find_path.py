fe5e64af2b676f140b902e719f55b04a2807f619
Magiz0r <fomag924@gmail.com>
2026-04-07 21:53:52 -0700

    disc5

---
+++ week6/disc5/find_path.py
@@ -0,0 +1,25 @@
+from tree import *
+
+def find_path(t, x):
+    """
+    >>> t2 = tree(5, [tree(6), tree(7)])
+    >>> t1 = tree(3, [tree(4), t2])
+    >>> find_path(t1, 5)
+    [3, 5]
+    >>> find_path(t1, 4)
+    [3, 4]
+    >>> find_path(t1, 6)
+    [3, 5, 6]
+    >>> find_path(t2, 6)
+    [5, 6]
+    >>> print(find_path(t1, 2))
+    None
+    """
+    if x == label(t):
+        return [x]
+    for branch in branches(t):
+        path = find_path(branch, x)
+        if path:
+            return [t[0]] + path
+    return None
+

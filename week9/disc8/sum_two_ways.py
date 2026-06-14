c0208b98e9cfeb7da31005c4101d8162330f4b5d
Magiz0r <fomag924@gmail.com>
2026-06-14 13:46:01 -0700

    week9

---
+++ week9/disc8/sum_two_ways.py
@@ -0,0 +1,87 @@
+def sum_rec(s, k):
+    """Return the sum of the first k elements in s.
+
+    >>> a = Link(1, Link(6, Link(8)))
+    >>> sum_rec(a, 2)
+    7
+    >>> sum_rec(a, 5)
+    15
+    >>> sum_rec(Link.empty, 1)
+    0
+    """
+    # Use a recursive call to sum_rec; don't call sum_iter
+    "*** YOUR CODE HERE ***"
+    if s is Link.empty or k == 0:
+        return 0
+    total = 0
+    
+    while k != 0:
+        total = s.first
+        return total + sum_rec(s.rest, k-1)
+
+def sum_iter(s, k):
+    """Return the sum of the first k elements in s.
+
+    >>> a = Link(1, Link(6, Link(8)))
+    >>> sum_iter(a, 2)
+    7
+    >>> sum_iter(a, 5)
+    15
+    >>> sum_iter(Link.empty, 1)
+    0
+    """
+    # Don't call sum_rec or sum_iter
+    "*** YOUR CODE HERE ***"
+    if s is Link.empty or k == 0:
+        return 0
+    total = 0
+    
+    while k != 0:
+        if s.rest is Link.empty:
+            total += s.first
+            return total
+        total += s.first
+        s = s.rest
+        k -= 1
+        
+    return total
+    
+
+
+
+
+
+
+class Link:
+    """A linked list is either a Link object or Link.empty
+
+    >>> s = Link(3, Link(4, Link(5)))
+    >>> s.rest
+    Link(4, Link(5))
+    >>> s.rest.rest.rest is Link.empty
+    True
+    >>> s.rest.first * 2
+    8
+    >>> print(s)
+    <3 4 5>
+    """
+    empty = ()
+
+    def __init__(self, first, rest=empty):
+        assert rest is Link.empty or isinstance(rest, Link)
+        self.first = first
+        self.rest = rest
+
+    def __repr__(self):
+        if self.rest:
+            rest_repr = ', ' + repr(self.rest)
+        else:
+            rest_repr = ''
+        return 'Link(' + repr(self.first) + rest_repr + ')'
+
+    def __str__(self):
+        string = '<'
+        while self.rest is not Link.empty:
+            string += str(self.first) + ' '
+            self = self.rest
+        return string + str(self.first) + '>'
